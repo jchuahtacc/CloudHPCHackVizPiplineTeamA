781f4337689a395657576d907a6d72a4bd90b65f
TheNotBotBot <59490993+TheNotBotBot@users.noreply.github.com>
2020-07-29 12:57:03 -0400

    adding data transform code, output csvs, and requirements file

---
+++ datatransform/InputData.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# reading in values from web hosted csv
+df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+
+# prints unmodified data frame
+print(df)
+
+# saves csv with no modifications
+df.to_csv(r'CovidData.csv')
+
+# strips data frame down to the necessary date field
+df = df.drop(["state", "fips", "cases", "deaths"], axis=1)
+
+# removes all duplicate data values, sorts them, and then prints modified date frame
+df = pd.DataFrame(data = df['date'].unique())
+print(df)
+
+# saves csv with only unique dates from the imported csv
+df.to_csv(r'CovidDates.csv')
