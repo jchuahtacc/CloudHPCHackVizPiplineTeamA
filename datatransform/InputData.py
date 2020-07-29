19f3dd467fb851f8830754acbd8c06f220602162
TheNotBotBot <59490993+TheNotBotBot@users.noreply.github.com>
2020-07-29 13:13:28 -0400

    updated file names for output csvs

--- datatransform/InputData.py
+++ datatransform/InputData.py
@@ -7,14 +7,14 @@ df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master
 print(df)
 
 # saves csv with no modifications
-df.to_csv(r'CovidData.csv')
+df.to_csv(r'data.csv')
 
 # strips data frame down to the necessary date field
-df = df.drop(["state", "fips", "cases", "deaths"], axis=1)
+#df = df.drop(["state", "fips", "cases", "deaths"], axis=1)
 
-# removes all duplicate data values, sorts them, and then prints modified date frame
+# removes all duplicate data values and then prints modified date frame
 df = pd.DataFrame(data = df['date'].unique())
 print(df)
 
 # saves csv with only unique dates from the imported csv
-df.to_csv(r'CovidDates.csv')
+df.to_csv(r'dates.csv')
